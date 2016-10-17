822125e0be8084a4e8f2be6e10287f297d407b2b
yuxizhe <yuxizhe2008@163.com>
2016-10-17 15:32:40 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var Koa = require('koa')
+var sha1 = require('sha1')
+var config = {
+    wechat: {
+        appID: 'wx1d4fb4342b55ba47',
+        appSecret: '16bccc0daabb60ac0e34bd6c99575bb9',
+        token: 'yuxizhe'
+    }
+}
+
+var app = new Koa()
+
+app.use(function*(next) {
+    console.log(this.query)
+})
+
+app.listen(3000)
