046dc1ada82a78428c94ff5f5c2d2a783b954684
yuxizhe <yuxizhe2008@163.com>
2016-10-17 15:53:33 +0800

    first commit

--- index.js
+++ index.js
@@ -16,6 +16,19 @@ app.use(function*(next) {
     console.log(this.query)
     var token = config.wechat.token
     var signature = this.query.signature
+    var nonce = this.query.nonce
+    var timestamp = this.query.timestamp
+    var ecostr = this.query.ecostr
+
+    var str = [token, timestamp, nonce].sort().join('')
+    var sha = sha1(str)
+
+    if (sha === signature) {
+        this.body = ecostr + ''
+    } else {
+        this.body = 'wrong'
+    }
 })
+
 
 app.listen(3000)
